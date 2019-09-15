67214a63b230b79d98105cd0791309a93a31d59d
Antidote <antidotedefacer@gmail.com>
2019-09-15 19:02:56 +0300

    Fix XSS module +Add minimal module

--- src/lib/php/core.php
+++ src/lib/php/core.php
@@ -13,7 +13,7 @@ function ListSubtitle($path)
     sort($imgArray);
     $c = count($imgArray);
     for ($i = 0; $i < $c; $i++) {
-        echo "<option value=\"" . xss_clean($imgArray[$i]) . "\">" . xss_clean($imgArray[$i]) . "\n";
+        echo "<option value=\"" . sanitize_xss($imgArray[$i]) . "\">" . sanitize_xss($imgArray[$i]) . "\n";
     }
 } // Function ListSubtitle
 
@@ -32,41 +32,9 @@ function delete_files($target)
     }
 } // Delete  Files Target Dictonary
 
-function xss_clean($data)
-{
-    // Fix &entity\n;
-    $data = str_replace(array(
-        '&amp;',
-        '&lt;',
-        '&gt;'
-    ), array(
-        '&amp;amp;',
-        '&amp;lt;',
-        '&amp;gt;'
-    ), $data);
-    $data = preg_replace('/(&#*\w+)[\x00-\x20]+;/u', '$1;', $data);
-    $data = preg_replace('/(&#x*[0-9A-F]+);*/iu', '$1;', $data);
-    $data = html_entity_decode($data, ENT_COMPAT, 'UTF-8');
-    // Remove any attribute starting with "on" or xmlns
-    $data = preg_replace('#(<[^>]+?[\x00-\x20"\'])(?:on|xmlns)[^>]*+>#iu', '$1>', $data);
-    // Remove javascript: and vbscript: protocols
-    $data = preg_replace('#([a-z]*)[\x00-\x20]*=[\x00-\x20]*([`\'"]*)[\x00-\x20]*j[\x00-\x20]*a[\x00-\x20]*v[\x00-\x20]*a[\x00-\x20]*s[\x00-\x20]*c[\x00-\x20]*r[\x00-\x20]*i[\x00-\x20]*p[\x00-\x20]*t[\x00-\x20]*:#iu', '$1=$2nojavascript...', $data);
-    $data = preg_replace('#([a-z]*)[\x00-\x20]*=([\'"]*)[\x00-\x20]*v[\x00-\x20]*b[\x00-\x20]*s[\x00-\x20]*c[\x00-\x20]*r[\x00-\x20]*i[\x00-\x20]*p[\x00-\x20]*t[\x00-\x20]*:#iu', '$1=$2novbscript...', $data);
-    $data = preg_replace('#([a-z]*)[\x00-\x20]*=([\'"]*)[\x00-\x20]*-moz-binding[\x00-\x20]*:#u', '$1=$2nomozbinding...', $data);
-    // Only works in IE: <span style="width: expression(alert('Ping!'));"></span>
-    $data = preg_replace('#(<[^>]+?)style[\x00-\x20]*=[\x00-\x20]*[`\'"]*.*?expression[\x00-\x20]*\([^>]*+>#i', '$1>', $data);
-    $data = preg_replace('#(<[^>]+?)style[\x00-\x20]*=[\x00-\x20]*[`\'"]*.*?behaviour[\x00-\x20]*\([^>]*+>#i', '$1>', $data);
-    $data = preg_replace('#(<[^>]+?)style[\x00-\x20]*=[\x00-\x20]*[`\'"]*.*?s[\x00-\x20]*c[\x00-\x20]*r[\x00-\x20]*i[\x00-\x20]*p[\x00-\x20]*t[\x00-\x20]*:*[^>]*+>#iu', '$1>', $data);
-    // Remove namespaced elements (we do not need them)
-    $data = preg_replace('#</*\w+:\w[^>]*+>#i', '', $data);
-    do {
-        // Remove really unwanted tags
-        $old_data = $data;
-        $data     = preg_replace('#</*(?:applet|b(?:ase|gsound|link)|embed|frame(?:set)?|i(?:frame|layer)|l(?:ayer|ink)|meta|object|s(?:cript|tyle)|title|xml)[^>]*+>#i', '', $data);
-    } while ($old_data !== $data);
-    // we are done...
-    return $data;
-} // xss clean
+function sanitize_xss($value) {
+	return htmlspecialchars(strip_tags($value));
+}
 
 function Translate($filename)
 {
@@ -78,7 +46,7 @@ function Translate($filename)
            foreach ($file_lines as $line) {
                $linex .= $line;
            }
-            $linew = xss_clean($linex);
+            $linew = sanitize_xss($linex);
             return preg_replace("/(^[\r\n]*|[\r\n]+)[\s\t]*[\r\n]+/", "\n", $linew);
         } else {
             return "";
